3e71ba0246507dfed24985557d308acf1cd4c838
Florian Müller <florian.mueller81@gmx.de>
2017-09-22 12:43:49 +0200

    Build processing is using 'production' environment

--- helpers/environment.js
+++ helpers/environment.js
@@ -23,7 +23,7 @@ if (argv.test) {
   process.env.NODE_ENV = ENV_KEY_TEST;
 }
 
-if (argv.production) {
+if (argv.production || argv.release || argv['release-simple']) {
   process.env.NODE_ENV = ENV_KEY_PRODUCTION;
 }
 
